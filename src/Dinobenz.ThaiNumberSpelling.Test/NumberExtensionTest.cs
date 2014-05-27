bfa34cc04a9e58429cda335793dbff98ed441b71
Pakorn Weecharungsan <dinobenz@hotmail.com>
2014-05-27 22:55:21 +0700

    First version
    Support integer

---
+++ src/Dinobenz.ThaiNumberSpelling.Test/NumberExtensionTest.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dinobenz.ThaiNumberSpelling.Test
+{
+    [TestClass]
+    public class NumberExtensionTest
+    {
+        int number;
+
+        [TestMethod]
+        public void Zero()
+        {
+            number = 0;
+
+            var expected = "ศูนย์";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            number = 1;
+
+            var expected = "หนึ่ง";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            number = 2;
+
+            var expected = "สอง";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            number = 3;
+
+            var expected = "สาม";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Eleven()
+        {
+            number = 11;
+
+            var expected = "สิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwentyOne()
+        {
+            number = 21;
+
+            var expected = "ยี่สิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneHundredEleven()
+        {
+            number = 111;
+
+            var expected = "หนึ่งร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoHundredEleven()
+        {
+            number = 211;
+
+            var expected = "สองร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneThousandOneHundredEleven()
+        {
+            number = 1111;
+
+            var expected = "หนึ่งพันหนึ่งร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TenThousandOneHundredEleven()
+        {
+            number = 11111;
+
+            var expected = "หนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneHundredThousandOneHundredEleven()
+        {
+            number = 111111;
+
+            var expected = "หนึ่งแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneMillionOneHundredThousandOneHundredElevenEleven()
+        {
+            number = 1111111;
+
+            var expected = "หนึ่งล้านหนึ่งแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ด";
+            var actual = number.ToThaiSpelling();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ src/Dinobenz.ThaiNumberSpelling/NumberExtension.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+
+namespace Dinobenz.ThaiNumberSpelling
+{
+    /// <summary>
+    /// The number extension class.
+    /// </summary>
+    public static class NumberExtension
+    {
+        /// <summary>
+        /// Convert number to Thai spelling word.
+        /// </summary>
+        /// <param name="value">The number</param>
+        /// <returns>The Thai spelling word</returns>
+        public static string ToThaiSpelling(this int value)
+        {
+            StringBuilder ret = new StringBuilder();
+            string[] digits = { "", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+            string[] units = { "", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน" };
+
+            for (int i = 0; i < value.ToString().Length; i++)
+            {
+                if (value.ToString()[i].ToString() != "0")
+                {
+                    if (i == value.ToString().Length - 1)
+                    {
+                        switch (value.ToString()[i].ToString())
+                        {
+                            case "1":
+                                if (ret.Length > 0)
+                                {
+                                    if (value.ToString()[i - 1].ToString() == "0")
+                                    {
+                                        ret.Append(digits[Convert.ToInt32(value.ToString()[i].ToString())]);
+                                    }
+                                    else
+                                    {
+                                        ret.Append("เอ็ด");
+                                    }
+                                }
+                                else
+                                {
+                                    ret.Append(digits[Convert.ToInt32(value.ToString()[i].ToString())]);
+                                }
+                                break;
+                            default:
+                                ret.Append(digits[Convert.ToInt32(value.ToString()[i].ToString())]);
+                                break;
+                        }
+                    }
+                    else if (i == value.ToString().Length - 2 || i == value.ToString().Length % 6 - 2)
+                    {
+                        switch (value.ToString()[i].ToString())
+                        {
+                            case "1":
+                                ret.Append("");
+                                break;
+                            case "2":
+                                ret.Append("ยี่");
+                                break;
+                            default:
+                                ret.Append(digits[Convert.ToInt32(value.ToString()[i].ToString())]);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        ret.Append(digits[Convert.ToInt32(value.ToString()[i].ToString())]);
+                    }
+
+                    if (value.ToString().Length - 1 - i >= units.Length)
+                    {
+                        ret.Append(units[value.ToString().Length % units.Length - i]);
+                    }
+                    else
+                    {
+                        ret.Append(units[value.ToString().Length - 1 - i]);
+                    }
+                }
+                else
+                {
+                    if (ret.Length == 0)
+                    {
+                        ret.Append("ศูนย์");
+                    }
+                }
+            }
+
+            return ret.ToString();
+        }
+    }
+}
